5bd6337752dfa43634fecb33d0674db8483e6aec
VitorPiovezan <vitor.piovezan@hotmail.com.br>
2020-08-21 16:36:31 -0300

    adicionado chamado get na screen room.js

--- frontend-tabler/src/components/Room/room.js
+++ frontend-tabler/src/components/Room/room.js
@@ -1,14 +1,16 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState, Component } from 'react';
 
 import {
     ContainerRoom,
 } from './styles';
 
+import api from '../../api/api'
 import { StyleSheet, ImageBackground, Text } from 'react-native';
 
 export default function Room({navigation, route}) {
 
     const idSala  = route.params.roomID  
+    const [rooms, setRooms] = useState([])
 
     const styles = StyleSheet.create({
         backgroundImage: {
@@ -28,10 +30,21 @@ export default function Room({navigation, route}) {
         }
     });
 
+    useEffect(() => {
+        async function getIdRoom() {
+                const res = await api.get(`roomData/${idSala}`)
+                setRooms(res.data)
+                console.log(res.data)
+        }
+        
+        getIdRoom()
+
+    }, [idSala])
+
     return (
         <ContainerRoom>
             <ImageBackground source={require('../../assets/images/fundo.png')} style={styles.backgroundImage} >
-    <Text>{idSala}</Text>
+                <Text>{idSala}</Text>
             </ImageBackground>
         </ContainerRoom>
 
